b11d21e8c7071e6d74b87da390ecae5263d90329
Satyam7sa <satyamjaiswalsatyamjaiswal93@gmail.com>
2026-01-23 14:38:44 +0530

    hollow

--- code.cpp
+++ code.cpp
@@ -4,8 +4,30 @@ using namespace std;
 int main()
 {
 
-    int n = 8;
+    // int n = 8;
 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // space
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+
+    //     // num 1
+    //     for (int j = 1; j <= i + 1; j++)
+    //     {
+    //         cout << j;
+    //     }
+    //     // num 2
+    //     for (int j = i; j > 0; j--)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    int n = 4;
     for (int i = 0; i < n; i++)
     {
         // space
@@ -13,16 +35,36 @@ int main()
         {
             cout << " ";
         }
-
-        // num 1
-        for (int j = 1; j <= i + 1; j++)
+        cout << "*";
+        if (i != 0)
         {
-            cout << j;
+            // spaces
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
         }
-        // num 2
-        for (int j = i; j > 0; j--)
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        // space
+
+        for (int j = 0; j < i+1; j++)
         {
-            cout << j;
+            cout << " ";
+        }
+        cout << "*";
+        if (i != n - 2)
+        {
+            // space
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
         }
         cout << endl;
     }
