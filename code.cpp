656d7aa721905085608e73c262a029352dafdb43
Satyam7sa <satyamjaiswalsatyamjaiswal93@gmail.com>
2026-01-27 06:41:05 +0530

    Array

--- code.cpp
+++ code.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int sumofDigits(int num){
-    int digitSum = 0;
-
-    while(num > 0){
-        int lastDigit = num % 10;
-        num /= 10;
-
-        digitSum += lastDigit;
+void reverseArray(int arr[], int size){
+    int start = 0,end= size-1;
+    while (start < end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
     }
-
-    return digitSum;
+    
 }
 
 int main(){
+    int arr[]= {2,3,4,5,6,7,8};
+    int size = 7;
 
-    cout << "sum = " << sumofDigits(2356) << endl;
+    reverseArray(arr,size);
+
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
